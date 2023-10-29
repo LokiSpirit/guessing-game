52174eb2174cffaab2a363336a4e0ab429f76b3b
Olga Krukova <krukovaolga1@gmail.com>
2023-10-29 15:59:59 +0200

    feat: add logic to all methods of class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if (this.min < this.max) {
+            this.mid = Math.ceil(this.min / 2 + this.max / 2);
+        }
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
